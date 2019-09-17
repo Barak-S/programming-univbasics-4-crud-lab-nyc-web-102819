a74ea8339817e5afb98fa358b64ac0c8e17a7eb9
Barak Saidoff <barak.saidoff@gmail.com>
2019-09-17 10:11:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  new_array = [1,2,3,4]
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array << 5
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
